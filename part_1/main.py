df85976938aaf48969f3ea2773e862e39a7db3d9
vauq2901 <quentin.vaucher.de.la.croix@usherbrooke.ca>
2017-11-18 12:56:46 -0500

    update repository

--- part_1/main.py
+++ part_1/main.py
@@ -1,30 +1,28 @@
-documents = {} # Document : [list of words]
+from multiprocessing import Process
+
+documents = {}  # Document : [list of words]
 words = {}
 
-results = [] # frequents words
+results = []  # frequents words
 
 for i in range(1, 4):
     with open('../docwords' + str(i) + '.txt', 'r', encoding='utf-8') as fin:
         for line in fin:
             line_array = line.split(" ")
-            if line_array[0] not in documents.keys(): # Check if the document already exists
+            if line_array[0] not in documents.keys():  # Check if the document already exists
                 documents[line_array[0]] = []
-            documents[line_array[0]].append(line_array[1]) # Add the word to the document
+            documents[line_array[0]].append(line_array[1])  # Add the word to the document
             if line_array[1] not in words.keys():
-                words[line_array[1]] = [0,0]
+                words[line_array[1]] = [0, 0]
             words[line_array[1]][0] += int(line_array[2][:-1])
             words[line_array[1]][1] += 1
-
 
 print(len(documents))
 
 print("reading done\n")
 
-
-
 for word, freq in words.items():
     results.append([word, (freq[0] * (len(documents)) / freq[1])])
-
 
 results.sort(key=lambda x: x[1])
 results.reverse()
@@ -33,15 +31,22 @@ print("results found\nstart writing\n")
 
 words_reduced = []
 
-with open('../results.txt','w',encoding='utf-8') as fou:
-    for i in range(200):
+with open('../results.txt', 'w', encoding='utf-8') as fou:
+    for i in range(500):
         words_reduced.append(results[i][0])
-        fou.write(str(results[i][0])+"\n")
+        fou.write(str(results[i][0]) + "\n")
+
+def process_docwords_i(i):
+    with open('../docwords' + str(i) + '.txt', 'r', encoding='utf-8') as fin:
+        for line in fin:
+            line_array = line.split(' ')
+            if line_array[1] in words_reduced:
+                fou.write(line)
+    print("End of file " + str(i))
+
 
 with open('../docwordsreduced.txt', 'w', encoding='utf-8') as fou:
     for i in range(1, 4):
-        with open('../docwords' + str(i) + '.txt', 'r', encoding='utf-8') as fin:
-            for line in fin:
-                line_array = line.split(' ')
-                if line_array[1] in words_reduced:
-                    fou.write(line)
+        p = Process(target=process_docwords_i, args=(i, ))
+        p.start()
+        p.join()
--- part_2/main.py
+++ part_2/main.py
@@ -1,7 +1,7 @@
 import math
 import random
 import numpy
-
+import _thread
 
 #############
 #  Classes  #
@@ -54,10 +54,10 @@ def calculate_number_of_documents():
     return len(set)
 
 
-def init_vectors(number_of_documents, vector_size):
-    """initialize a vector (D=5000) for each documents filled with 0."""
+def init_vectors(vector_size):
+    """initialize a vector (D=vector_size) for each documents filled with 0."""
     vectors = {}
-    for j in range(number_of_documents):
+    for j in range(len(docwords)):
         doc_id = docwords[j].split(" ")[0]
         vectors[doc_id] = Vector()
         for i in range(vector_size):
@@ -86,23 +86,13 @@ def update_vectors(vectors):
         vectors[array_line[0]].update_dimension(index, dimension)
 
 
-def cosine_similarity(v1, v2):
-    """compute the cosine similarity between two vectors."""
-    sum_dot = 0
-    sum_norm_v1 = 0
-    sum_norm_v2 = 0
+def euclidean_distance(v1, v2):
+    sum = 0
 
     for i in range(len(v1)):
-        sum_dot += v1[i] * v2[i]
-        sum_norm_v1 += v1[i] * v1[i]
-        sum_norm_v2 += v2[i] * v2[i]
+        sum += math.pow((v1[i] - v2[i]), 2)
 
-    sum_norm = sum_norm_v1 * sum_norm_v2
-
-    if sum_norm != 0:
-        return math.acos(sum_dot / math.sqrt(sum_norm))
-    else:
-        return math.inf
+    return math.sqrt(sum)
 
 
 def update_clusters_centers(clusters):
@@ -111,7 +101,7 @@ def update_clusters_centers(clusters):
 
     for cluster in clusters:
         new_cluster = []
-        for i in range(number_of_documents):
+        for i in range(number_of_documents): # create new cluster filled with zeroes
             new_cluster.append(0)
 
         number_of_vector = 0
@@ -124,21 +114,20 @@ def update_clusters_centers(clusters):
                     new_cluster[index] += dimension
                     index += 1
 
-            if number_of_vector != 0:
-                for i in range(len(cluster)):  # divide the sum by the number of vector in order to get the mean value
-                    new_cluster[i] = new_cluster[i] / number_of_vector
+        if number_of_vector != 0:
+            for i in range(len(cluster)):  # divide the sum by the number of vector in order to get the mean value
+                new_cluster[i] = new_cluster[i] / number_of_vector
 
         new_clusters.append(new_cluster)
 
     is_stable = True
     for i in range(len(clusters)):
-        # print(cosine_similarity(clusters[i], new_clusters[i]) - 0.04)
-        if cosine_similarity(clusters[i], new_clusters[i]) > 0.04:
-            isStable = False
+        distance = euclidean_distance(clusters[i], new_clusters[i])
+        print(distance)
+        if distance > numpy.finfo(float).eps:
+            is_stable = False
 
-    clusters = new_clusters
-
-    return is_stable
+    return new_clusters, is_stable
 
 
 def print_vector(vector):
@@ -158,23 +147,27 @@ def k_means(k, vectors):
 
     stable = False
     while not stable:
+        print("K-Means")
         for vector in vectors.values():
             for cluster in clusters:
-                dist = cosine_similarity(vector.dimensions, cluster)
+                dist = euclidean_distance(vector.dimensions, cluster)
 
                 if dist < vector.dist:
                     vector.cluster = cluster
 
-        stable = update_clusters_centers(clusters)
+        clusters, stable = update_clusters_centers(clusters)
 
-    if k < 10:
-        with open('output-clusters.txt', 'w', encoding='utf-8') as fou:
-            for out_cluster in clusters:
-                for key,vector in vectors.items():
-                    if vector.cluster == out_cluster:
-                        fou.write(key+"\n")
 
-                fou.write("\n" + "-"*20 + "\n")
+    with open('output-clusters.txt', 'w', encoding='utf-8') as fou:
+        for out_cluster in clusters:
+            empty = True
+            for key, vector in vectors.items():
+                if vector.cluster == out_cluster:
+                    fou.write(key+"\n")
+                    empty = False
+
+            if not empty:
+                fou.write("\n" + "-"*50 + "\n")
 
 
 ##########
@@ -190,11 +183,10 @@ number_of_documents = calculate_number_of_documents()
 vector_size = len(results)
 
 print("Init vectors")
-vectors = init_vectors(number_of_documents, vector_size)
+vectors = init_vectors(vector_size)
 update_vectors(vectors)
-
+2
 print("Start K-Means")
 random.seed()
-nb_clusters = 8
+nb_clusters = 14000
 k_means(nb_clusters, vectors)
-
